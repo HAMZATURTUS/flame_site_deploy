55a63103f12abf818e544a2264b6207accaf88da
Hamza Turtus <selnaji13@protonmail.com>
2025-04-23 21:06:33 +0300

    members page updated

--- flame_site/members/models.py
+++ flame_site/members/models.py
@@ -6,8 +6,9 @@ class Member(models.Model):
     last_name = models.CharField(max_length=30)
     description = models.CharField(max_length=500)
     image = models.ImageField(upload_to='portraits/', default='portraits/placeholder.png')
-    linkedin_url = models.CharField(max_length=100)
+    linkedin = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    github = models.CharField(max_length=100)
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
