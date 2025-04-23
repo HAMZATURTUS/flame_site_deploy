26df43b2820e4be52dcf95e8e0792fc930b61bc0
Hamza Turtus <selnaji13@protonmail.com>
2025-04-24 02:36:16 +0300

    member page renovated

--- flame_site/members/models.py
+++ flame_site/members/models.py
@@ -1,14 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class University(models.Model):
+    name = models.CharField(max_length=50)
+    initials = models.CharField(max_length=10)
+    logo = models.ImageField(upload_to='uni_logos/', default='portraits/placeholder.png')
+    
+    def __str__(self):
+        return f"{self.name}"
+
+class Major(models.Model):
+    name = models.CharField(max_length=50)
+    initials = models.CharField(max_length=5)
+    
+    def __str__(self):
+        return f"{self.name}"
+
 class Member(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    description = models.CharField(max_length=500)
     image = models.ImageField(upload_to='portraits/', default='portraits/placeholder.png')
-    linkedin = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    github = models.CharField(max_length=100)
+    university = models.ForeignKey(University, on_delete=models.CASCADE, null=True)
+    major = models.ForeignKey(Major, on_delete=models.CASCADE, null=True)
+    linkedin = models.CharField(max_length=100, null=True)
+    github = models.CharField(max_length=100, null=True)
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
