e1506bab293e292bfb2cfc7e338ac4260b61b86b
HAMZATURTUS <145903357+HAMZATURTUS@users.noreply.github.com>
2025-04-25 19:01:27 +0300

    Delete flame_site directory

--- flame_site/blogs/context_processors.py
+++
@@ -1,9 +0,0 @@
-# blogs/context_processors.py
-from .models import Blog
-
-def first_blog_url(request):
-    # Fetch the blog URL (adjust the title filter as needed)
-    blog = Blog.objects.filter(title="Welcome to our Blogs!").live().first()
-    return {
-        'first_blog_url': blog.url if blog else None,
-    }
--- flame_site/writeups/wagtail_hooks.py
+++
@@ -1,8 +0,0 @@
-from wagtailcache.cache import clear_cache
-from wagtail import hooks
-
-@hooks.register('after_create_page')
-@hooks.register('after_edit_page')
-def clear_wagtailcache(request, page):
-    if page.live:
-        clear_cache()  # Clears the Wagtail cache for the entire site
